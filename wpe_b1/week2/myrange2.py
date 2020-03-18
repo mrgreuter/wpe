53e4f84610bda20441e61bbec1b35f41a2d05d1f
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-18 07:41:25 +0100

    wip

--- wpe_b1/week2/myrange2.py
+++ wpe_b1/week2/myrange2.py
@@ -3,17 +3,29 @@ class myrange2:
     def __init__(self, range, max_range=0, step_size=1):
         if max_range != 0 and max_range < range:
             raise ValueError
-        self.range = range
-        self.max_range = max_range
+        elif max_range != 0:
+            self.start = range
+            self.end = max_range
+        else:
+            self.start = 0
+            self.end = range
+        
         self.step_size = step_size
 
     def __iter__(self):
-        self.result = 0
+        self.result = self.start
         return self
 
     def __next__(self):
-        if self.max_range > self.range:
-            self.result = 
-        if self.resul
+        if self.result > self.end:
+            self.result += self.step_size
+            return result
         else:
             StopIteration
+
+
+my_iter = iter(myrange2(5))
+my_iter2 = iter(myrange2(5, 10))
+my_iter3 = iter(myrange2(0, 42, 7))
+
+print(next(my_iter))
