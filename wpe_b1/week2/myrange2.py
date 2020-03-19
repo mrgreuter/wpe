96cdf9bcc11aefab675b9003f9213fd5782a49f2
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-19 18:06:29 +0100

    myrange2 finished

--- wpe_b1/week2/myrange2.py
+++ wpe_b1/week2/myrange2.py
@@ -1,37 +1,24 @@
 class myrange2:
 
-    def __init__(self, range, max_range=0, step_size=1):
+    def __new__(cls, range, max_range=0, step_size=1):
+
         if max_range != 0 and max_range < range:
             raise ValueError
         elif max_range != 0:
-            self.start = range
-            self.end = max_range
+            cls.start = range
+            cls.end = max_range
         else:
-            self.start = 0
-            self.end = range
+            cls.start = 0
+            cls.end = range
         
-        self.step_size = step_size
+        cls.step_size = step_size
 
-    def __iter__(self):
-        self.result = self.start
-        return self
+        cls.range_list = []
+        current_val = cls.start
+        while current_val < cls.end:
+            cls.range_list.append(current_val)
+            current_val += step_size
+        return cls.range_list
 
-    def __next__(self):
-        if self.result < self.end:
-            self.result += self.step_size
-            return self.result
-        else:
-            StopIteration
-
-a = myrange2(5)
-my_iter = iter(a)
-
-#my_iter2 = iter(myrange2(5, 10))
-#my_iter3 = iter(myrange2(0, 42, 7))
-
-print(next(my_iter))
-print(next(my_iter))
-print(next(my_iter))
-print(next(my_iter))
-print(next(my_iter))
-print(next(my_iter))
+new_list = myrange2(5,20,3)
+print(new_list)
