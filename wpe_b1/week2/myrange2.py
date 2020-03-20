daec351f69e625b2460883461699ae7b37859ec8
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-20 19:04:57 +0100

    wip

--- wpe_b1/week2/myrange2.py
+++ wpe_b1/week2/myrange2.py
@@ -1,24 +1,27 @@
 class myrange2:
 
-    def __new__(cls, range, max_range=None, step_size=None):
+    def __new__(cls, start, steps=None, step_size=None):
 
-        if max_range != None and max_range < range:
-            raise ValueError
-        elif max_range != 0:
-            cls.start = range
-            cls.end = max_range
-        else:
-            cls.start = 0
-            cls.end = range
-        
         if step_size != None:
             cls.step_size = step_size
         else:
             cls.step_size = 1
 
+        if steps != None:
+            cls.steps = steps
+        else:
+            cls.steps = start
+
+        if steps == None:
+            cls.start = 0
+            cls.end = start
+        else:
+            cls.start = start
+            cls.end = steps
+        
         cls.range_list = []
         current_val = cls.start
         while current_val < cls.end:
             cls.range_list.append(current_val)
-            current_val += step_size
+            current_val += cls.step_size
         return cls.range_list
