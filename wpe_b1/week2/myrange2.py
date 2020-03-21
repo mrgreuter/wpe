0fd670d0ad75a0f6aaca83329969fe23a611fd68
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-21 07:52:22 +0100

    fin 2

--- wpe_b1/week2/myrange2.py
+++ wpe_b1/week2/myrange2.py
@@ -1,21 +1,17 @@
 class myrange2:
 
-    def __new__(cls, start, max=None, step_size=None):
+    def __new__(cls, start, max=None, step_size=1):
 
         if max != None:
             cls.max = max
             cls.start = start
-            cls.end = max
         else:
             cls.max = start
             cls.start = 0
-            cls.end = start
-
-        if step_size != None:
-            cls.step_size = step_size
-        else:
-            cls.step_size = 1
         
+        cls.end = cls.max
+        cls.step_size = step_size
+
         cls.range_list = []
         cls.current_value = cls.start
         while cls.current_value < cls.end:
--- wpe_b1/week2/myrange3.py
+++ wpe_b1/week2/myrange3.py
@@ -1,20 +1,16 @@
 class myrange3:
 
-    def __new__(cls, start, max=None, step_size=None):
+    def __new__(cls, start, max=None, step_size=1):
 
         if max != None:
             cls.max = max
             cls.start = start
-            cls.end = max
         else:
             cls.max = start
             cls.start = 0
-            cls.end = start
         
-        if step_size != None:
-            cls.step_size = step_size
-        else:
-            cls.step_size = 1
+        cls.end = cls.max
+        cls.step_size = step_size
 
         cls.current_value = cls.start
         while cls.current_value < cls.end:
