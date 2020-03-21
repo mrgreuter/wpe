74b4bac0b3a252189b9e50f4b6fa86a4eb098c19
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-21 07:40:24 +0100

    final version exercise 2

--- wpe_b1/week2/myrange2.py
+++ wpe_b1/week2/myrange2.py
@@ -1,27 +1,25 @@
 class myrange2:
 
-    def __new__(cls, start, steps=None, step_size=None):
+    def __new__(cls, start, max=None, step_size=None):
+
+
+        if max != None:
+            cls.max = max
+            cls.start = start
+            cls.end = max
+        else:
+            cls.max = start
+            cls.start = 0
+            cls.end = start
 
         if step_size != None:
             cls.step_size = step_size
         else:
             cls.step_size = 1
-
-        if steps != None:
-            cls.steps = steps
-        else:
-            cls.steps = start
-
-        if steps == None:
-            cls.start = 0
-            cls.end = start
-        else:
-            cls.start = start
-            cls.end = steps
         
         cls.range_list = []
-        current_val = cls.start
-        while current_val < cls.end:
-            cls.range_list.append(current_val)
-            current_val += cls.step_size
+        cls.current_value = cls.start
+        while cls.current_value < cls.end:
+            cls.range_list.append(cls.current_value)
+            cls.current_value += cls.step_size
         return cls.range_list
--- wpe_b1/week2/myrange3.py
+++ wpe_b1/week2/myrange3.py
@@ -1,24 +1,22 @@
 class myrange3:
 
-    def __new__(cls, start, steps=None, step_size=None):
+    def __new__(cls, start, max=None, step_size=None):
 
+
+        if max != None:
+            cls.max = max
+            cls.start = start
+            cls.end = max
+        else:
+            cls.max = start
+            cls.start = 0
+            cls.end = start
+        
         if step_size != None:
             cls.step_size = step_size
         else:
             cls.step_size = 1
 
-        if steps != None:
-            cls.steps = steps
-        else:
-            cls.steps = start
-
-        if steps == None:
-            cls.start = 0
-            cls.end = start
-        else:
-            cls.start = start
-            cls.end = steps
-        
         cls.current_value = cls.start
         while cls.current_value < cls.end:
             yield cls.current_value
