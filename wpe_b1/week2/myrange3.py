5f738f98e8dd07cbc6b18c5d64ac73c0fad6ab0f
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-21 07:26:28 +0100

    done

--- wpe_b1/week2/myrange3.py
+++ wpe_b1/week2/myrange3.py
@@ -1,27 +1,25 @@
 class myrange3:
 
-    def __new__(cls, range, max_range=None, step_size=None):
+    def __new__(cls, start, steps=None, step_size=None):
 
-        if max_range != None and max_range < range:
-            raise ValueError
-        elif max_range != None:
-            cls.start = range
-            cls.end = max_range
-        else:
-            cls.start = 0
-            cls.end = range
-
-        cls.current_value = cls.start
-        
-        if step_size != None: 
+        if step_size != None:
             cls.step_size = step_size
         else:
             cls.step_size = 1
 
+        if steps != None:
+            cls.steps = steps
+        else:
+            cls.steps = start
+
+        if steps == None:
+            cls.start = 0
+            cls.end = start
+        else:
+            cls.start = start
+            cls.end = steps
+        
+        cls.current_value = cls.start
         while cls.current_value < cls.end:
             yield cls.current_value
             cls.current_value += cls.step_size
-
-
-for x in myrange3(5):
-    print(x)
