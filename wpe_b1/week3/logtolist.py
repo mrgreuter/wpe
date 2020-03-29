d46c47d6c1ed114650754a23d08650bcb0f8f45a
Daniel Mitterecker <d.mitterecker@student.vu.nl>
2020-03-29 13:21:53 +0200

    wip

--- wpe_b1/week3/logtolist.py
+++ wpe_b1/week3/logtolist.py
@@ -1,39 +1,59 @@
 import re
-from collections import defaultdict
 import os
 
 def logtolist(filename: str) -> dict:
-    log_pattern = '^(?P<ip>.*?)\s(.*?)\s(.*?)\s\[(?P<timestamp>.*?)\]\s"(?P<request>.*?)"\s(?P<http_status_code>.*?)\s(?P<size>.*?)\s"(.*?)"\s"(?P<user_agent>.*?)"'
-    compiled_log_pattern = re.compile(log_pattern)
+    
+    log_line_pattern = r'''^(?P<ip_address>.*?)\s
+                                    (.*?)\s
+                                    (.*?)\s
+                                    \[(?P<timestamp>.*?)\]\s
+                                    "(?P<request>.*?)"\s
+                                    (.*?)\s
+                                    (.*?)\s
+                                    "(.*?)"\s
+                                    "(.*?)"'''
+    compiled_log_line_pattern = re.compile(log_line_pattern)
+    ip_log_pattern = r'\d+.\d+.\d+.\d+'
+    compiled_ip_log_pattern = re.compile(ip_log_pattern)
+    timestamp_log_pattern = r'\d+/\w+/\d+:\d+:\d+:\d+\s\+\d+'
+    compiled_timestamp_log_pattern = re.compile(timestamp_log_pattern)
+    request_log_pattern = r'"GET.*?"'
+    compiled_request_log_pattern = re.compile(request_log_pattern)
+
     log_list = []
     # for tests
-    if hasattr(filename, 'getvalue'):
-        line_count = 0
-        for line in filename.getvalue():
-            line_count += 1
-            line_match = re.search(compiled_log_pattern, line)
-            if line_match:
-                log_list.append({
-                    "ip_address" : line_match.group('ip'),
-                    "timestamp" : line_match.group('timestamp'),
-                    "request" : line_match.group('request'),
-                })
+    content = filename.getvalue()
+    content = content.strip('\n')
+    file_content_list = (content).split('\n')
+    
+    for line in file_content_list:
+        valid_log_line = re.search(compiled_log_line_pattern, line)
+        if valid_log_line:
+            log_list.append({
+                "ip_address" : valid_log_line.group('ip_address'),
+                "timestamp" : valid_log_line.group('timestamp'),
+                "request" : valid_log_line.group('request'),
+            })
+        else:
+            ip_address = re.search(compiled_ip_log_pattern, line)
+            if ip_address:
+                ip_address = ip_address.group(0)
             else:
-                log_list.append({})
-    else:
-        script_path = os.path.dirname(__file__)
-        file_path = os.path.join(script_path, filename)
-        with open(file_path) as log_file:
-            line_count = 0
-            for line in log_file:
-                line_count += 1
-                line_match = re.search(compiled_log_pattern, line)
-                if line_match:
-                    log_list.append({
-                        "ip_address" : line_match.group('ip'),
-                        "timestamp" : line_match.group('timestamp'),
-                        "request" : line_match.group('request'),
-                    })
-                else:
-                    print(f"\n!!! Log on line {line_count} not valid. Skipping line. !!!\n")
+                ip_address = "No IP address found"
+            
+            timestamp = re.search(compiled_timestamp_log_pattern, line)
+            if timestamp:
+                timestamp = timestamp.group(0)
+            else:
+                timestamp = "No timestamp found"
+            
+            request = re.search(compiled_request_log_pattern, line)
+            if request:
+                request = request.group(0)[1:-1]
+            else:
+                request = "No request found"
+
+            log_list.append({"ip_address": ip_address, 
+                            "timestamp": timestamp, 
+                            "request": request})
     return log_list
